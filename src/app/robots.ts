23504ef5842a0016d5462a23bf88db0034c35c4f
jarisk <jaroslavs.kuznecovs@gmail.com>
2026-03-14 18:25:36 -0400

    Add dynamic sitemap and robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+const BASE_URL = "https://buyherepayhere.io";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+    host: BASE_URL,
+  };
+}
